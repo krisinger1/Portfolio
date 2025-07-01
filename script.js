9c62382d3d9e38f5eac7a3944d554b61768ab204
Kristin <krisinger1@gmail.com>
2025-06-30 22:36:17 -0500

    add contact page and js interactivity on project pictures

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function learnMore(){
-    document.querySelector('.hidden').style.visibility = 'visible';
+function addBorder(event){
+    let t = event.target;
+    // t.style.display = 'none';
+    t.style.border = '5px solid #048C8C';
+    t.style.borderRadius = '10px';
+    console.log(t);
+    console.log("i moused over this picture!")
 }
+function removeBorder(event){
+    let t = event.target;
+    t.style.border = 'none';
+    t.style.borderRadius = '0px';
+
+
+
+}
+
+let projectImages = document.querySelectorAll('.project-image');
+console.log(projectImages);
+projectImages.forEach(image => {    
+    image.addEventListener('mouseover', addBorder);
+    image.addEventListener('mouseout', removeBorder);
+});
